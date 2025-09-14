3dcc7968ab00221e2df8f0ab069f8d9394dd5ee1
askmesupu <ask.me.pirated@gmail.com>
2025-09-14 14:48:07 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AnswerQuestion from "./pages/AnswerQuestion";
-import MyLoveEnglish from "./pages/MyLoveEnglish";
-import MyLoveBangla from "./pages/MyLoveBangla";
-import MyLoveHindi from "./pages/MyLoveHindi";
-import MyHeart from "./pages/MyHeart";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MyLoveEnglish from './pages/MyLoveEnglish';
+import MyLoveBangla from './pages/MyLoveBangla';
+import MyLoveHindi from './pages/MyLoveHindi';
+import MyHeart from './pages/MyHeart';
+import AnswerQuestion from './pages/AnswerQuestion';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div
-        style={{
-          minHeight: "100vh",
-          width: "100%",
-          backgroundImage: `
-            url("https://i.imgur.com/8sdznQ1.jpeg"),
-            url("https://i.imgur.com/OGgciNc.png")
-          `,
-          backgroundSize: "cover, 200px",
-          backgroundPosition: "center, bottom right",
-          backgroundRepeat: "no-repeat, no-repeat",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<AnswerQuestion />} />
-          <Route path="/love-en" element={<MyLoveEnglish />} />
-          <Route path="/love-bn" element={<MyLoveBangla />} />
-          <Route path="/love-hi" element={<MyLoveHindi />} />
-          <Route path="/heart" element={<MyHeart />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/english" element={<MyLoveEnglish />} />
+        <Route path="/bangla" element={<MyLoveBangla />} />
+        <Route path="/hindi" element={<MyLoveHindi />} />
+        <Route path="/myheart" element={<MyHeart />} />
+        <Route path="/answer" element={<AnswerQuestion />} />
+      </Routes>
     </Router>
   );
 }
