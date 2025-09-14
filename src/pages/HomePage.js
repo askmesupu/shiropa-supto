2b587e13b36de8702783db4300dfd61affc5383c
askmesupu <ask.me.pirated@gmail.com>
2025-09-14 16:15:47 +0600

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import bubu from '../assets/bubu.png';
+import dudu from '../assets/dudu.png';
 
 const HomePage = () => {
   return (
@@ -7,7 +9,9 @@ const HomePage = () => {
       <p>
         Our journey began in 2023, the moment I first laid eyes on Shiropa. From that day, I whispered heartfelt prayers to the universe, hoping that fate would unite us. Time passed, and with patience and faith, our hearts intertwined. Finally, after the beautiful celebration of Durga Puja, on the 20th of October 2025, our love blossomed fully, and we officially became a couple. Every moment since then has been a precious journey of love, laughter, and endless dreams together.
       </p>
-      <p className="heart">❤️</p>
+      <img src={bubu} alt="Bubu" className="floating-img" style={{top:'10%', left:'10%'}}/>
+      <img src={dudu} alt="Dudu" className="floating-img" style={{top:'20%', left:'70%'}}/>
+      <button className="button">Start Journey</button>
     </div>
   );
 };
