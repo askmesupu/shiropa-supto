26f3d19e9e17685a8d14bb0f6c35dc903ad698ff
askmesupu <ask.me.pirated@gmail.com>
2025-09-14 16:20:58 +0600

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import bubu from '../assets/bubu.png';
-import dudu from '../assets/dudu.png';
 
 const HomePage = () => {
   return (
@@ -9,8 +7,21 @@ const HomePage = () => {
       <p>
         Our journey began in 2023, the moment I first laid eyes on Shiropa. From that day, I whispered heartfelt prayers to the universe, hoping that fate would unite us. Time passed, and with patience and faith, our hearts intertwined. Finally, after the beautiful celebration of Durga Puja, on the 20th of October 2025, our love blossomed fully, and we officially became a couple. Every moment since then has been a precious journey of love, laughter, and endless dreams together.
       </p>
-      <img src={bubu} alt="Bubu" className="floating-img" style={{top:'10%', left:'10%'}}/>
-      <img src={dudu} alt="Dudu" className="floating-img" style={{top:'20%', left:'70%'}}/>
+      
+      {/* Bubu & Dudu pics using online URLs */}
+      <img 
+        src="https://i.imgur.com/8sdznQ1.jpeg" 
+        alt="Bubu" 
+        className="floating-img" 
+        style={{top:'10%', left:'10%'}} 
+      />
+      <img 
+        src="https://i.imgur.com/OGgciNc.png" 
+        alt="Dudu" 
+        className="floating-img" 
+        style={{top:'20%', left:'70%'}} 
+      />
+
       <button className="button">Start Journey</button>
     </div>
   );
