56672280ce9ecccd9b8d0d8be79e00fa954b9e5a
askmesupu <ask.me.pirated@gmail.com>
2025-09-14 15:42:43 +0600

    Update AnswerQuestion.js

--- src/pages/AnswerQuestion.js
+++ src/pages/AnswerQuestion.js
@@ -1,75 +1,66 @@
-import React, { useState, useEffect } from "react";
-import "./AnswerQuestion.css";
+import React, { useState } from "react";
 
-function AnswerQuestion() {
-  const [bgColor, setBgColor] = useState("#ffcccc");
+const AnswerQuestion = () => {
+  const [yesPopup, setYesPopup] = useState(false);
+  const [noPopup, setNoPopup] = useState(false);
+  const [noPos, setNoPos] = useState({ top: "50%", left: "50%" });
 
-  useEffect(() => {
-    const colors = ["#ffcccc", "#ffe6cc", "#ccffcc", "#ccffff", "#ccccff", "#ffccff"];
-    let i = 0;
-    const interval = setInterval(() => {
-      setBgColor(colors[i]);
-      i = (i + 1) % colors.length;
-    }, 2000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const handleNoClick = (e) => {
-    const button = e.target;
-    const x = Math.random() * (window.innerWidth - button.offsetWidth);
-    const y = Math.random() * (window.innerHeight - button.offsetHeight);
-    button.style.left = `${x}px`;
-    button.style.top = `${y}px`;
-    button.classList.add("shake");
-    setTimeout(() => button.classList.remove("shake"), 500);
-
-    const noPopup = document.createElement("img");
-    noPopup.src = "https://i.imgur.com/tpIx24B.png";
-    noPopup.style.position = "absolute";
-    noPopup.style.width = "150px";
-    noPopup.style.left = `${x}px`;
-    noPopup.style.top = `${y - 150}px`;
-    noPopup.style.zIndex = "999";
-    document.body.appendChild(noPopup);
-    setTimeout(() => noPopup.remove(), 1000);
+  const handleYes = () => {
+    setYesPopup(true);
+    setTimeout(() => setYesPopup(false), 3000);
   };
 
-  const handleYesClick = () => {
-    alert("I love you baby ❤️");
-
-    const yesPopup = document.createElement("img");
-    yesPopup.src = "https://i.imgur.com/nXvrTyh.jpeg";
-    yesPopup.style.position = "absolute";
-    yesPopup.style.width = "200px";
-    yesPopup.style.left = "50%";
-    yesPopup.style.top = "50%";
-    yesPopup.style.transform = "translate(-50%, -50%)";
-    yesPopup.style.zIndex = "999";
-    document.body.appendChild(yesPopup);
-    setTimeout(() => yesPopup.remove(), 2000);
+  const handleNoHover = () => {
+    const top = Math.random() * 70 + "%";
+    const left = Math.random() * 70 + "%";
+    setNoPos({ top, left });
+    setNoPopup(true);
+    setTimeout(() => setNoPopup(false), 2000);
   };
 
   return (
-    <div
-      className="answer-container"
-      style={{
-        backgroundColor: bgColor,
-        backgroundImage:
-          "url('https://i.imgur.com/8sdznQ1.jpeg'), url('https://i.imgur.com/OGgciNc.png')",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-      }}
-    >
-      <h2>Will you stay forever with me?</h2>
-      <button id="yesBtn" onClick={handleYesClick}>
-        Yes
-      </button>
-      <button id="noBtn" onClick={handleNoClick}>
-        No
-      </button>
+    <div className="container" style={{ position: "relative", minHeight: "50vh" }}>
+      <h1>Will you stay forever with me?</h1>
+      <div style={{ marginTop: "30px", position: "relative", height: "150px" }}>
+        <button
+          onClick={handleYes}
+          style={{ padding: "10px 20px", fontSize: "1em", marginRight: "20px" }}
+        >
+          Yes
+        </button>
+        <button
+          onMouseEnter={handleNoHover}
+          style={{
+            position: "absolute",
+            top: noPos.top,
+            left: noPos.left,
+            padding: "10px 20px",
+            fontSize: "1em"
+          }}
+        >
+          No
+        </button>
+      </div>
+
+      {yesPopup && (
+        <img
+          src="https://i.imgur.com/nXvrTyh.jpeg"
+          alt="Yes"
+          className="popup-img"
+          style={{ display: "block" }}
+        />
+      )}
+
+      {noPopup && (
+        <img
+          src="https://i.imgur.com/tpIx24B.png"
+          alt="No"
+          className="popup-img"
+          style={{ display: "block" }}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default AnswerQuestion;
